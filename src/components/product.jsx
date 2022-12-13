5fd81a7a0c3275f309418000b0a2375a46289d46
Jim Barnett <hlbradow@yahoo.com>
2022-12-12 22:44:22 -0500

    final-final

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,17 +1,23 @@
 import "./product.css";
 import QuantityPicker from "./quantityPicker";
+import { useState } from "react";
 
 function Product(props) {
+    const [quantity, setQuantity] = useState(1);
+    function addTotal(qty) {
+        console.log("Price Total", qty);
+        setQuantity(qty);
+    }
     return (
         <div className="product">
             <img src={props.data.image} alt="products" />
             <h5>{props.data.title}</h5>
             <h6>{props.data.author}</h6>
             <div className="prices">
-                <label>Total:${props.data.price.toFixed(2)}</label>
+                <label>Total:${props.data.price.toFixed(2) * quantity}</label>
                 <label>Price:${props.data.price.toFixed(2)}</label>
             </div>
-            <QuantityPicker />
+            <QuantityPicker onChange={addTotal} />
             <button>Add to Cart</button>
         </div>
     );
