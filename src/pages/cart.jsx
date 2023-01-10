d80d7903f2f80217fd9412e53dc319c2e6c83411
Jim Barnett <hlbradow@yahoo.com>
2023-01-09 22:54:49 -0500

    109 class 2.1

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -2,6 +2,10 @@ function Cart() {
     return (
         <div className="cart">
             <h1>Payment</h1>
+            <img
+                src="https://sewhitebooks.com/wp-content/uploads/2019/05/bookworm-gif.gif"
+                alt="Book Wizard"
+            ></img>
         </div>
     );
 }
